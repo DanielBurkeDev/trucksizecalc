85bb547fa6558750340300ac2fd2e22829cf069e
DanielBurkeDev <danielburkedev@gmail.com>
2025-05-04 12:14:12 +0100

    added some affiliate links

--- script.js
+++ script.js
@@ -7,17 +7,11 @@ const getSize = function () {
   const deckSizeEl = document.getElementById("deckSize").value;
   const truckBrandEl = document.getElementById("truckBrand").value;
   const truckSizeResult = document.getElementById("trucksize-res");
-  let truckSize;
-  let truckSizeResultText;
 
-  // MATCH SELECTED VALUE WITH PROPERTY WITHIN OBJECT
+  const truckData = truckSizes[truckBrandEl]?.[deckSizeEl];
 
-  truckSize = truckSizes[truckBrandEl][deckSizeEl] || "Unknown";
-
-  if (truckSize === "Unknown") {
-    truckSizeResultText = `There is no match with that Deck Size and Truck Brand.`;
-
-    truckSizeResult.innerText = truckSizeResultText;
+  if (!truckData) {
+    truckSizeResult.innerText = `There is no match with that Deck Size and Truck Brand.`;
     truckSizeResult.classList.remove(
       "d-block",
       "animate__animated",
@@ -30,9 +24,12 @@ const getSize = function () {
       "animate__bounceIn"
     );
   } else {
-    truckSizeResultText = `The size suitable for ${deckSizeEl}" deck is ${truckBrandEl} ${truckSize}.`;
-
-    truckSizeResult.innerText = truckSizeResultText;
+    const { model, link } = truckData;
+    console.log(truckData);
+    truckSizeResult.innerHTML = `
+      The size suitable for ${deckSizeEl}" deck is <strong>${truckBrandEl} ${model}</strong>.<br>
+      Amazon Link: <a href="${link}" target="_blank" rel="noopener noreferrer">Click here to view/purchase ${truckBrandEl} trucks</a>
+    `;
     truckSizeResult.classList.remove(
       "d-block",
       "animate__animated",
@@ -45,27 +42,71 @@ const getSize = function () {
       "animate__bounceIn"
     );
   }
-
-  // if (truckBrandObj.hasOwnProperty(deckSizeElVal)) {
-  //   console.log(truckBrandObj[deckSizeElVal], "and", deckSizeElVal);
-  //   let truckSizeResultText = `The size suitable for ${deckSizeElVal}" deck is ${truckBrandElVal} ${truckBrandObj[deckSizeElVal]}.`;
-  //   truckSizeResult.innerText = truckSizeResultText;
-  //   truckSizeResult.classList.add(
-  //     "d-block",
-  //     "animate__animated",
-  //     "animate__fadeIn"
-  //   );
-  // } else {
-  //   console.log(truckBrandObj[deckSizeElVal], "and", deckSizeElVal);
-  //   let truckSizeResultText = `There is no match for those sizes.`;
-  //   truckSizeResult.innerText = truckSizeResultText;
-  //   truckSizeResult.classList.add(
-  //     "d-block",
-  //     "animate__animated",
-  //     "animate__fadeIn"
-  //   );
-  // }
 };
+
+// const getSize = function () {
+//   const deckSizeEl = document.getElementById("deckSize").value;
+//   const truckBrandEl = document.getElementById("truckBrand").value;
+//   const truckSizeResult = document.getElementById("trucksize-res");
+//   let truckSize;
+//   let truckSizeResultText;
+
+//   // MATCH SELECTED VALUE WITH PROPERTY WITHIN OBJECT
+
+//   truckSize = truckSizes[truckBrandEl][deckSizeEl] || "Unknown";
+
+//   if (truckSize === "Unknown") {
+//     truckSizeResultText = `There is no match with that Deck Size and Truck Brand.`;
+
+//     truckSizeResult.innerText = truckSizeResultText;
+//     truckSizeResult.classList.remove(
+//       "d-block",
+//       "animate__animated",
+//       "animate__bounceIn"
+//     );
+//     void truckSizeResult.offsetWidth;
+//     truckSizeResult.classList.add(
+//       "d-block",
+//       "animate__animated",
+//       "animate__bounceIn"
+//     );
+//   } else {
+//     truckSizeResultText = `The size suitable for ${deckSizeEl}" deck is ${truckBrandEl} ${truckSize}.`;
+
+//     truckSizeResult.innerText = truckSizeResultText;
+//     truckSizeResult.classList.remove(
+//       "d-block",
+//       "animate__animated",
+//       "animate__bounceIn"
+//     );
+//     void truckSizeResult.offsetWidth;
+//     truckSizeResult.classList.add(
+//       "d-block",
+//       "animate__animated",
+//       "animate__bounceIn"
+//     );
+//   }
+
+// if (truckBrandObj.hasOwnProperty(deckSizeElVal)) {
+//   console.log(truckBrandObj[deckSizeElVal], "and", deckSizeElVal);
+//   let truckSizeResultText = `The size suitable for ${deckSizeElVal}" deck is ${truckBrandElVal} ${truckBrandObj[deckSizeElVal]}.`;
+//   truckSizeResult.innerText = truckSizeResultText;
+//   truckSizeResult.classList.add(
+//     "d-block",
+//     "animate__animated",
+//     "animate__fadeIn"
+//   );
+// } else {
+//   console.log(truckBrandObj[deckSizeElVal], "and", deckSizeElVal);
+//   let truckSizeResultText = `There is no match for those sizes.`;
+//   truckSizeResult.innerText = truckSizeResultText;
+//   truckSizeResult.classList.add(
+//     "d-block",
+//     "animate__animated",
+//     "animate__fadeIn"
+//   );
+// }
+// };
 
 // theButtonEl.addEventListener("click", checkSizes);
 theButtonEl.addEventListener("click", getSize);
